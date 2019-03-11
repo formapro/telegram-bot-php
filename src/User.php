fbeeadd429be91e4c7c62fbfeab5299b21086048
Max Kotliar <kotlyar.maksim@gmail.com>
2019-03-11 18:01:49 +0200

    add user methods.

--- src/User.php
+++ src/User.php
@@ -4,6 +4,9 @@ namespace Formapro\TelegramBot;
 
 use function Formapro\Values\get_value;
 
+/**
+ * https://core.telegram.org/bots/api#user
+ */
 class User
 {
     private $values = [];
@@ -13,5 +16,30 @@ class User
     public function getId(): int
     {
         return get_value($this, 'id');
+    }
+
+    public function isBot(): bool
+    {
+        return get_value($this, 'is_bot');
+    }
+
+    public function getFirstName(): string
+    {
+        return get_value($this, 'first_name');
+    }
+
+    public function getLastName(): ?string
+    {
+        return get_value($this, 'last_name');
+    }
+
+    public function getUsername(): ?string
+    {
+        return get_value($this, 'username');
+    }
+
+    public function getLanguageCode(): ?string
+    {
+        return get_value($this, 'language_code');
     }
 }
